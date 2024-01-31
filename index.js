f5d718c667129ed821dbd2294481b2efb1172723
YakovGorbunov <158257817+YakovGorbunov@users.noreply.github.com>
2024-01-31 12:05:11 +0500

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(1);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click' , function (){
+    output.innerHTML = 'ВЫ САМЫЕ ЛУЧШИЕ НА СВЕТЕ И В МИРE И ВООБЩЕ ❤️';
+});
